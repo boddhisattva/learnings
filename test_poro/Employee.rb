57dbc6e8d81d64824dbb98150d9f310396a0faf3
Mohnish G J <mail4mohnish@gmail.com>
2015-02-17 07:54:50 +0530

    uses a gemfile and adds pry support for debugging

--- test_poro/Employee.rb
+++ test_poro/Employee.rb
@@ -1,3 +1,10 @@
-  def total_score(scores)
-    total_score = scores.inject(:+)
-  end
+# require 'bundler'
+# require 'bundler/setup'
+# Bundler.setup(:default, :test, :development)
+
+Bundler.require(:default, :development, :test)
+
+def total_score(scores)
+  binding.pry
+  scores.inject(:+)
+end
